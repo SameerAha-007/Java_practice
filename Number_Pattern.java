afbd2d890019ca11f8704a453f1a5d90dc363dd3
shaik Sameer Ahamad <143081982+SameerAha-007@users.noreply.github.com>
2024-06-30 22:24:16 +0530

    Update Number_Pattern.java

--- Number_Pattern.java
+++ Number_Pattern.java
@@ -1,7 +1,7 @@
 /*output
- *1 2 3
- *4 5 6
- *7 8 9
+ 1 2 3
+ 4 5 6
+ 7 8 9
 */
 public class Number_Pattern
 {
@@ -19,9 +19,9 @@ public class Number_Pattern
 
 
 /*output
- *9 8 7
- *6 5 4
- *3 2 1
+9 8 7
+6 5 4
+3 2 1
 */
 public class Number_Pattern
 {
@@ -38,9 +38,9 @@ public class Number_Pattern
 }
 
 /*output
- *1 2 3
- *2 4 6
- *3 6 9
+ 1 2 3
+ 2 4 6
+ 3 6 9
 */
 public class Number_Pattern
 {
@@ -50,6 +50,26 @@ public class Number_Pattern
 		    for (int j=1;j<=3;j++){ 
 		        k=j*i;
 		        System.out.print(k+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+/*output
+1
+12
+123
+1234
+12345
+*/
+public class Star_pattern
+{
+	public static void main(String[] args) {
+	    int k=1;
+		for(int i=1;i<=5;i++){
+		    for (int j=1;j<=i;j++){
+		        System.out.print(j);
 		    }
 		    System.out.println();
 		}
