8ceaaf431508c63af06ebb6cc5d991cb2e83c120
shaik Sameer Ahamad <143081982+SameerAha-007@users.noreply.github.com>
2024-06-30 22:00:20 +0530

    3

---
+++ Star_pattern.java
@@ -0,0 +1,89 @@
+/*public class Star_pattern
+{
+	public static void main(String[] args) {
+	    int k=1;
+		for(int i=1;i<=5;i++){
+		    for (int j=1;j<=i;j++){
+		        System.out.print("*"+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
+public class Star_pattern
+{
+	public static void main(String[] args) {
+	    
+		for(int i=1;i<=5;i++){
+		    for (int j=i;j<=5;j++){
+		        System.out.print("*"+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+public class Star_pattern
+{
+	public static void main(String[] args) {
+	    int k=5;
+		for(int i=1;i<=k;i++){
+		    for (int l=1;l<=k-i;l++){
+		        System.out.print(" ");
+		    }
+		    for (int j=1;j<=i;j++){
+		       System.out.print("*"+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+public class Star_pattern
+{
+	public static void main(String[] args) {
+	    int k=5;
+		for(int i=1;i<=k;i++){
+		    for (int l=1;l<=k-i;l++){
+		        System.out.print(" "+" ");
+		    }
+		    for (int j=1;j<=i;j++){
+		       System.out.print("*"+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+public class Star_pattern
+{
+	public static void main(String[] args) {
+	    int k=1;
+		for(int i=1;i<=5;i++){
+		    for (int j=1;j<=i;j++){
+		        System.out.print(j);
+		    }
+		    System.out.println();
+		}
+	}
+}
+*/
+
+
+public class Star_pattern
+{
+	public static void main(String[] args) {
+	    int k=5;
+		for(int i=1;i<=k;i++){
+		    for (int l=1;l<=k-i;l++){
+		        System.out.print(" "+" ");
+		    }
+		    for (int j=1;j<=(2*i)-1;j++){
+		       System.out.print("*"+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
