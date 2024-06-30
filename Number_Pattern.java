735d1d8ab0da6bb4e235ca966824a3616034d01b
shaik Sameer Ahamad <143081982+SameerAha-007@users.noreply.github.com>
2024-06-30 21:54:24 +0530

    2

---
+++ Number_Pattern.java
@@ -0,0 +1,57 @@
+/*output
+ *1 2 3
+ *4 5 6
+ *7 8 9
+*/
+public class Number_Pattern
+{
+	public static void main(String[] args) {
+	    int k=1;
+		for(int i=1;i<=3;i++){
+		    for (int j=1;j<=3;j++){
+		        System.out.print(k+" ");
+		        k++;
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+
+/*output
+ *9 8 7
+ *6 5 4
+ *3 2 1
+*/
+public class Number_Pattern
+{
+	public static void main(String[] args) {
+	    int k=9;
+		for(int i=1;i<=3;i++){
+		    for (int j=1;j<=3;j++){
+		        System.out.print(k+" ");
+		        k--;
+		    }
+		    System.out.println();
+		}
+	}
+}
+
+/*output
+ *1 2 3
+ *2 4 6
+ *3 6 9
+*/
+public class Number_Pattern
+{
+	public static void main(String[] args) {
+	    int k=1;
+		for(int i=1;i<=3;i++){
+		    for (int j=1;j<=3;j++){ 
+		        k=j*i;
+		        System.out.print(k+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
