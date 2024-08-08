cba594c603bd3dc484f0a33bc7682b85cab95461
shaik Sameer Ahamad <143081982+SameerAha-007@users.noreply.github.com>
2024-08-08 12:34:57 +0530

    5

---
+++ PerfectNumberOrNot.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PerfectNumberOrNot
+{
+
+	public static void main(String[] args) {
+
+	    Scanner sc=new Scanner(System.in);
+
+	    int num = sc.nextInt();
+
+		int sum=0;
+
+		int t=num;
+
+		for (int i=1;i<num;i++){
+
+		    if(num%i==0){
+
+		        sum+=i;
+
+		    }
+
+		}
+
+		if (sum==t){
+
+		    System.out.println(t+" is Perfect Number");
+
+		}
+
+		else{
+
+		    System.out.println(t+" is not Perfect Number");
+
+		}
+
+	}
+
+}
