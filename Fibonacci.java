efd27e34dcf9e06a8f728c8b65953e997eb20b82
SameerAha-007 <skshdjdhsj@gmail.com>
2025-02-20 21:32:40 +0530

    Range of fibonacii with sum

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,15 +2,18 @@ public class Fibonacci {
     static void fib(int n) {
         int p1 = 1;
         int p2 = 0;
-        System.out.print(p2 + " ");
-        System.out.print(p1 + " ");
+        System.out.print(p2+" ");
+        System.out.print(p1+" ");
+        int s=p1+p2;
         for (int i = 3; i <= n; i++) {
             int curr = p2 + p1;
             p2 = p1;
             p1 = curr;
             System.out.print(curr + " ");
+            s+=curr;
         }
         System.out.println();
+        System.out.println("Sum : "+s);
     }
     public static void main(String[] args) {
         fib(8);
@@ -18,3 +21,5 @@ public class Fibonacci {
     }
 
 }
+
+
